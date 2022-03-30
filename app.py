1bab50d88c5a8e5ebda36bafe90c978f0a46a46f
akhil <bolisetty.akhil33@gmail.com>
2022-03-30 19:32:26 +0530

    second commit

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 import os
 
 import aws_cdk as cdk
+from first_vpc.first_vpc_stack import FirstvpcStack
 
-from first_vpc.first_vpc_stack import FirstVpcStack
+
 
 
 app = cdk.App()
+
+
+
 FirstVpcStack(app, "FirstVpcStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
@@ -20,7 +24,7 @@ FirstVpcStack(app, "FirstVpcStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='804075638102', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- first_vpc/first_vpc_stack.py
+++ first_vpc/first_vpc_stack.py
@@ -1,8 +1,13 @@
-from aws_cdk import (
+from compileall import compile_dir
+from email.utils import encode_rfc2231
+import aws_cdk as cdk
+import aws_cdk.aws_ec2 as ec2
+from aws_cdk import  (
     # Duration,
     Stack,
     # aws_sqs as sqs,
 )
+
 from constructs import Construct
 
 class FirstVpcStack(Stack):
@@ -11,9 +16,20 @@ class FirstVpcStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(
+        self, "Myvpc",
+      #  cidr="10.13.0.0/21",
+     #   max_azs=2,
+    #    nat_gateways=0,
+   #     subnet_configuration=[
+  #          ec2.SubnetConfiguration=(name="PUBLIC", cidr_mask=24,subnet_type=ec2.SubnetType.PUBLIC),
+ #           ec2.SubnetConfiguration=(name="PRIVATE", cidr_mask=24,subnet_type=ec2.SubnetType.PRIVATE)
+#
+        #]    
+       # )
 
         # example resource
         # queue = sqs.Queue(
         #     self, "FirstVpcQueue",
         #     visibility_timeout=Duration.seconds(300),
-        # )
+         )
