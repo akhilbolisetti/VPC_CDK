6aeb9c74e746c244ea2fd6c37fd73e2b5928509a
akhil.bolisetti <bolisetty.akhil33@gmail.com>
2022-03-31 21:08:13 +0530

    second commit

--- first_vpc/first_vpc_stack.py
+++ first_vpc/first_vpc_stack.py
@@ -1,5 +1,8 @@
 from compileall import compile_dir
 from email.utils import encode_rfc2231
+from hashlib import new
+import string
+import this
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
 from aws_cdk import  (
@@ -16,8 +19,24 @@ class FirstVpcStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        vpc = ec2.Vpc(
+        my_vpc = ec2.Vpc(
         self, "Myvpc",
+        subnet_configuration=[
+            cidr="10.13.0.0/21"
+            ec2.SubnetConfiguration_public=(name="PUBLIC", cidr_mask=24,subnet_type=ec2.SubnetType.PUBLIC),
+            ec2.SubnetConfiguration_private=(name="PRIVATE", cidr_mask=24,subnet_type=ec2.SubnetType.PRIVATE)
+        ]
+        )
+
+        instance =  ec2.Instance(self, 'ec2-instance',instance_type=ec2.InstanceType('t2.micro'),machine_image=ec2.machine_image('ami-0c02fb55956c7d316')
+        vpc=my_vpc,subnet_configuration=ec2.SubnetConfiguration_public=(name="PUBLIC", cidr_mask=24,subnet_type=ec2.SubnetType.PUBLIC) )
+
+        instance =  ec2.Instance(self, 'ec2-instance',instance_type=ec2.InstanceType('t2.micro'),machine_image=ec2.machine_image('ami-0c02fb55956c7d317')
+        vpc=my_vpc,subnet_configuration=ec2.SubnetConfiguration_private=(name="PRIVATE", cidr_mask=24,subnet_type=ec2.SubnetType.PRIVATE) )
+
+        
+
+
       #  cidr="10.13.0.0/21",
      #   max_azs=2,
     #    nat_gateways=0,
